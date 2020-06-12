377e52b10277cf1d7fe279acd6404f1f827f9870
forkelt <thomasfolkertmol@gmail.com>
2020-06-12 23:03:17 +0200

    More error reporting, general improvements.

--- mytar.c
+++ mytar.c
@@ -5,11 +5,19 @@
 
 #define TAR_MAGIC "ustar" /* With null. */
 #define TAR_VERSION "00" /* No null. */
+#define OLD_MAGIC "ustar  " /* With null. */
+#define VERSION_LENGTH 2
 #define BLOCK_SIZE 512
-#define UNEXPECTED_EOF -1
+#define UNEXPECTED_EOF_CODE 2
+#define INVALID_HEADER_CODE 2
 
-#define UNKNOWN_OPTION "%s: unknown option %s\n"
-#define MISSING_OPTIONS "%s: need at least one option\n"
+#define UNKNOWN_OPTION "%s: Unknown option: %s\n"
+#define MISSING_OPTIONS "%s: Need at least one option\n"
+#define LONE_ZERO_BLOCK "%s: A lone zero block at %d\n"
+#define FILE_NOT_FOUND "%s: %s: Not found in archive\n"
+#define NOT_FOUND_FINAL "%s: Exiting with failure status due to previous errors\n"
+#define UNSUPPORTED_HEADER "%s: Unsupported header type: %d\n"
+#define UNEXPECTED_EOF "%s: Unexpected EOF in archive\n%s: Error is not recoverable: exiting now\n"
 
 
 struct header {
@@ -29,7 +37,7 @@ struct header {
 	char dev_major[8];
 	char dev_minor[8];
 	char prefix[155];
-	char pad[12]; /* For a clean 512 bytes, avoids certain issues. */
+	char pad[12]; /* For a clean 512 bytes; avoids certain issues. */
 };
 
 
@@ -45,45 +53,58 @@ int zero_block(void *memory)
 }
 
 
-int last_block(FILE *fp)
+int last_block(FILE *fp, char *prog, int blocks)
 {
 	char buffer[BLOCK_SIZE];
-
-	if (!fread(buffer, BLOCK_SIZE, 1, fp) || !zero_block((void*) &buffer))
-		return UNEXPECTED_EOF;
-	if (feof(fp))
-		return 0;
 	
-	return 1;
+	if (feof(fp))
+		fprintf(stderr, LONE_ZERO_BLOCK, prog, blocks);
+	else if (!fread(buffer, BLOCK_SIZE, 1, fp))
+		return UNEXPECTED_EOF_CODE;
+	else if (!zero_block((void*) &buffer))
+		fprintf(stderr, LONE_ZERO_BLOCK, prog, blocks + 1);
+	
+	return 0;
 }
 
 
-void block_align(FILE *fp)
+int block_align(FILE *fp)
 {
-	fseek(fp, (BLOCK_SIZE - ftell(fp) % BLOCK_SIZE) % BLOCK_SIZE, SEEK_CUR);
+	return fseek(fp, (BLOCK_SIZE - ftell(fp) % BLOCK_SIZE) % BLOCK_SIZE, 
+		     SEEK_CUR);
 }
 
 
-int read_headers(FILE *fp)
+int read_headers(FILE *fp, char *prog)
 {
 	struct header head;
 	long file_size;
+	int blocks;
 
 	for (;;) {
+		blocks = ftell(fp) / BLOCK_SIZE;
+
 		if (feof(fp))
 			return 0;
 
 		if (!fread(&head, sizeof(head), 1, fp))
-			return UNEXPECTED_EOF;
+			return UNEXPECTED_EOF_CODE;
 
 		if (zero_block((void*) &head))
-			return last_block(fp);
+			return last_block(fp, prog, blocks);
 
+		if (strcmp(head.magic, OLD_MAGIC) &&
+		    (strcmp(head.magic, TAR_MAGIC) ||
+		     strncmp(head.version, TAR_VERSION, VERSION_LENGTH))) {
+			fprintf(stderr, UNSUPPORTED_HEADER, prog, blocks);
+			return INVALID_HEADER_CODE;
+		}
+		
 		printf("%s\n", head.name);
 
 		sscanf(head.size, "%lo", &file_size);
-		fseek(fp, file_size, SEEK_CUR);
-		block_align(fp);
+		if (fseek(fp, file_size, SEEK_CUR) || block_align(fp))
+			return UNEXPECTED_EOF_CODE;
 	}
 }
 
@@ -110,7 +131,7 @@ int main(int argc, char *argv[])
 	}
 
 	FILE *fp = fopen(file, "rb");
-	int exit_code = read_headers(fp);
+	int exit_code = read_headers(fp, prog);
 	fclose(fp);
 	return exit_code;
 }
